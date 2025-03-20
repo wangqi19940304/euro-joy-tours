810a68b3145c27a8d16cda1527eeda25cd9662e7
wangqi57 <wangqi57@baidu.com>
2025-03-20 18:10:18 +0800

    feature:home develop

---
+++ miniprogram/pages/examples/constants.js
@@ -0,0 +1,27 @@
+const DiscoveryData = [
+  { 
+    id: '1',
+    picture: '../../images/discovery1.jpg',
+    name: '李梦君',
+    collect: 25,
+    gender: 2
+  },
+  { 
+    id: '2',
+    picture: '../../images/discovery2.jpg',
+    name: '杨君勇',
+    collect: 36,
+    gender: 1
+  },
+  { 
+    id: '3',
+    picture: '../../images/discovery3.jpg',
+    name: '刘汉亮',
+    collect: 48,
+    gender: 1
+  },
+];
+
+module.exports = {
+  DiscoveryData
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,10 +1,11 @@
 const { envList } = require("../../envList");
-const { QuickStartPoints, QuickStartSteps } = require("./constants");
+const { FeedData } = require("./constants");
 
 Page({
   data: {
-    knowledgePoints: QuickStartPoints,
-    steps: QuickStartSteps,
+    feedList: FeedData,
+    travelTabList: ['最近热门', '城市攻略', '主题旅行'],
+    travelTabClass: 0,
   },
 
   copyCode(e) {
@@ -28,9 +29,28 @@ Page({
     })
   },
 
-  gotoGoodsListPage() {
+  gotoTravelPage() {
     wx.navigateTo({
-      url: '/pages/goods-list/index',
+      url: '/pages/business/index',
+    })
+  },
+
+  gotoCustomPage() {
+    wx.navigateTo({
+      url: '/pages/travelCustomization/index',
+    })
+  },
+
+  gotoAiPage() {
+    wx.navigateTo({
+      url: '/pages/aiAssistant/index',
+    })
+  },
+
+  onTabClick(event) {
+    const index = event.currentTarget.dataset.index;
+    this.setData({
+      travelTabClass: index
     })
   },
 });
